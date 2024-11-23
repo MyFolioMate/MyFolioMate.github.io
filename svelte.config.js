ef2e23b7bab4118b6c3930f71ca9d8596af20bc6
Chris Jen Ian D. Roa <chrisianroa@gmail.com>
2024-11-23 11:41:59 +0800

    debug7

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
+			fallback: '404.html'
 		}),
 		paths: {
-			base: ''
+			base: process.env.NODE_ENV === 'production' ? '/MyFolioMate.github.io' : ''
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
