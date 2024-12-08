a5519a6a9f59318ebb8d786096185a578785086f
Chris Jen Ian D. Roa <chrisianroa@gmail.com>
2024-12-08 14:04:22 +0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: 'undefined',
 			precompress: false
 		}),
 		prerender: {
